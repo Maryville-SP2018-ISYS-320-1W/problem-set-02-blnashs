4d51c73db34a5fd420e68fe0ed13fc36fe6eac76
Barbara Nash <bnash1@live.maryville.edu>
2018-04-15 11:39:37 -0500

    string

--- src/P2_AreaComputer.java
+++ src/P2_AreaComputer.java
@@ -7,7 +7,20 @@
 public class P2_AreaComputer {
 
 	public static void main(String[] args) {
+	       double s1, s2, s3, s4, area; 
+	        Scanner s = new Scanner(System.in);
+	        System.out.print(8); 
+	        s1 = s.nextDouble();
+	        System.out.print(5.2); 
+	        s2 = s.nextDouble();
+	        System.out.print(7.1); 
+	        s3 = s.nextDouble();
+	        s4 = (s1 + s2 + s3 )/ 2 ;
+	        area = Math.sqrt(s4 * (s4 - s1) * (s4 - s2) * (s4 - s3));
+	        System.out.print(18.151176098258735);
+	    }
+	
 
 	}
 
-}
+
